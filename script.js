d8f507af07eeaf6f21d8f40455d1a35acec8b23b
Saiddidi <126246149+Saiddidi@users.noreply.github.com>
2024-09-28 16:50:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const countdown = () => {
+  const countDate = new Date("May 29, 2025 08:00:00").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(gap / day);
+  const hours = Math.floor((gap % day) / hour);
+  const minutes = Math.floor((gap % hour) / minute);
+  const seconds = Math.floor((gap % minute) / second);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (gap < 0) {
+    document.getElementById("countdown").innerHTML =
+      "<h2>Event has passed!</h2>";
+  }
+};
+
+setInterval(countdown, 1000);
